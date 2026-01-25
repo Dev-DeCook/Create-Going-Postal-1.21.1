510afd7609e2b3308457f0ca26bfa06d6c367c91
Dev-DeCook <decook@decook.net>
2026-01-25 18:00:07 +0000

    tidy

--- src/main/java/com/decook/creategoingpostal/block/custom/PostmastersDeskBlock.java
+++ src/main/java/com/decook/creategoingpostal/block/custom/PostmastersDeskBlock.java
@@ -5,7 +5,6 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
